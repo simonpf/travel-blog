37a582d3be9d4d831c39347201d91c44eb11180b
Simon Pfreundschuh <simonpf@chalmers.se>
2016-10-01 14:51:02 +0200

    Added missing file.

--- js/nav.js
+++ js/nav.js
@@ -14,7 +14,7 @@
             });
         } else if (scrollTop <= y_pos) {
             $navbar.removeClass("navbar-fixed").clearQueue().animate({
-                top: "-48px"
+                top: "0px"
             }, 0);
         }
     });
