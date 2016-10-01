69352696b51cf815182d69ab75722cca94acbe50
Simon Pfreundschuh <simonpf@chalmers.se>
2016-10-01 13:58:10 +0200

    Initial commit.

---
+++ js/map.js
@@ -0,0 +1,13 @@
+map = L.map('mapid').setView([51.505, -0.09], 10);
+var OpenTopoMap = L.tileLayer('http://.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+var tiles = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+tiles.addTo(map);
---
+++ js/nav.js
@@ -0,0 +1,21 @@
+(function($) {
+    "use strict";
+
+    var $navbar = $("#header"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+    console.log("hallo");
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos + height) {
+            $navbar.addClass("navbar-fixed").animate({
+                top: 0
+            });
+        } else if (scrollTop <= y_pos) {
+            $navbar.removeClass("navbar-fixed").clearQueue().animate({
+                top: "-48px"
+            }, 0);
+        }
+    });
+})(jQuery, undefined);
