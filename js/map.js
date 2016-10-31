d770984c261a82c62a8783ed46a2db2e8e799a5e
Simon Pfreundschuh <simonpf@chalmers.se>
2016-10-31 18:37:17 +0100

    First working version of the blog.

--- js/map.js
+++ js/map.js
@@ -1,13 +1,67 @@
-map = L.map('mapid').setView([51.505, -0.09], 10);
+map = L.map('mapid').setView([-24.0, -49.1], 8);
+map.scrollWheelZoom.disable();
 var OpenTopoMap = L.tileLayer('http://.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
-var tiles = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	subdomains: 'abcd',
+
+var osmUrl='/tiles/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var tiles = L.tileLayer(osmUrl, {
+    attribution: osmAttrib,
+    subdomains: 'abcd',
+    minZoom: 5,
+    maxZoom: 12
+});
+
+var stamenUrl    = 'http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}'
+var stamenAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+var stamenTiles = L.tileLayer(stamenUrl, {
+        attribution: stamenAttrib,
+        subdomains: 'abcd',
 	minZoom: 0,
 	maxZoom: 18,
 	ext: 'png'
 });
-tiles.addTo(map);
+stamenTiles.addTo(map);
+
+var photoLayer = L.photo.cluster({ spiderfyDistanceMultiplier: 1.2 });
+var onClickFunction = function (evt) {
+    evt.layer.bindPopup(L.Util.template('<img src="{url}" style="max-width: 100%; max-height: 100%px"/>', evt.layer.photo),
+                        {className: 'leaflet-popup-photo',
+                         minWidth:  300,
+                         maxHeight: 500
+                        }).openPopup();
+};
+photoLayer.on('click', onClickFunction);
+
+var options = {
+    color: "black",
+    weight: 5,
+    opacity: 0.5
+};
+var photos = [];
+var points = [];
+var polylines = [];
+
+for (i = 0; i < images.length; i++) {
+    photos.push([]);
+    points.push([]);
+    console.log(images[i][1].length)
+    for (j = 0; j < images[i][1].length; j++) {
+        img = images[i][1][j];
+        photos[i].push({
+            lat: img[1],
+            lng: img[2],
+            post_url: images[i][0],
+            url: img[0],
+            caption: "",
+            thumbnail: img[0].concat(".jpg")
+        });
+        points[i].push(new L.LatLng(img[1], img[2]));
+    }
+    photoLayer.add(photos[i]).addTo(map);
+    polylines.push(L.polyline(points[i], options));
+    map.addLayer(polylines[i]);
+}
+map.fitBounds(photoLayer.getBounds());
--- site.hs
+++ site.hs
@@ -1,9 +1,22 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+import           Data.Char
+import           Data.List
+import           Data.Either
+import           Data.Maybe
+import qualified Data.Map as M
 import           Data.Monoid (mappend)
+import           Data.String
 import           Hakyll
+import           Text.Pandoc.Definition
+import           Text.Pandoc.Walk
 import qualified Data.ByteString.Lazy.Char8 as C
+import           Debug.Trace
 import           Text.Jasmine
+import           Graphics.HsExif
+import           System.Process
+import           System.FilePath.Posix
 
 --------------------------------------------------------------------------------
 -- | Javascript compiler as found on
@@ -14,9 +27,91 @@ compressJsCompiler = do
   s <- getResourceString
   return $ itemSetBody (minifyJS s) s
 
+applyPandoc :: ([Block] -> [Block]) -> Pandoc -> Pandoc
+applyPandoc f (Pandoc h bs) = Pandoc h (f bs)
+
+liftImages :: Block -> [Block]
+liftImages (Para (i@(Image _ _ _):ls)) = (Div ("image", [], []) [Plain [i]]) : (liftImages (Para ls))
+liftImages (Para [])                   = []
+liftImages (Para ls)                   = Para ls' : liftImages (Para ls'')
+  where isImage (Image _ _ _) = True
+        isImage _             = False
+        ls'                   = takeWhile (not . isImage) ls
+        ls''                  = dropWhile (not . isImage) ls
+liftImages x = [x]
+
+transformHeaders :: [Block] -> [Block]
+transformHeaders ((Header l _ ((Str s):_)):bs) = (Div (s', [], []) bs') : transformHeaders bs''
+  where s'   = map toLower (take 2  s)
+        bs'  = takeWhile (not . isDivOrHeader) bs
+        bs'' = dropWhile (not . isDivOrHeader) bs
+        isDivOrHeader (Header l _ _)    = l == 1
+        isDivOrHeader (Div (s'', _, _) _) = s'' == "image"
+        isDivOrHeader _               = False
+transformHeaders (x:xs) = x : transformHeaders xs
+transformHeaders     [] = []
+
+groupDivs :: [Block] -> [Block]
+groupDivs (d@(Div (s, _, _) _):bs)
+  | s == "image" = d : groupDivs bs
+  | otherwise    = (Div ("float", [], []) (d:bs')) : (groupDivs bs'')
+  where bs'  = takeWhile (not . isSameHeaderOrImage) bs
+        bs'' = dropWhile (not . isSameHeaderOrImage) bs
+        isSameHeaderOrImage (Div (s', _, _) _) = (s == s') || (s' == "image")
+        isSameHeaderOrImage _                  = False
+groupDivs (x:xs) = x : groupDivs xs
+groupDivs []     = []
+
+customPandocCompiler :: Compiler (Item String)
+customPandocCompiler = do
+  pandoc <- getResourceBody >>= readPandoc
+  return $ writePandoc ((traceShowId (applyPandoc (groupDivs . transformHeaders . (concatMap liftImages)) <$> pandoc)))
+
+extractImagePathAndPos :: Inline -> String
+extractImagePathAndPos (Image _ _ t) = ",[\"" ++ fst t ++ "\"," ++ pos ++ "]"
+  where pos = (traceShowId (dropWhile (\x -> elem x ("fig:" :: [Char])) (snd t)))
+extractImagePathAndPos _             = []
+
+mInvert :: Monad a => [a b] -> a [b]
+mInvert [] = return []
+mInvert (a:aa) =  do
+  bb <- mInvert aa
+  b  <- a
+  return (b:bb)
+
+gpsPosCompiler :: Compiler (Item String)
+gpsPosCompiler = do
+  posts   <- recentFirst =<< loadAllSnapshots "posts/*" "raw_content"
+  let pandocs = mInvert $ map readPandoc posts
+  imgs    <-  map ((drop 1) . traceShowId . (query extractImagePathAndPos) . itemBody) <$> pandocs
+  names   <- (map (toFilePath . itemIdentifier)) <$> pandocs
+  makeItem $ "var images = [\n"
+             ++ unlines (intersperse "," (zipWith toJsList names imgs))
+             ++ "]"
+    where toJsList x y = "[\"" ++ x ++ "\", [" ++ y ++ "]]"
+
+thumbnailCompiler :: Compiler ()
+thumbnailCompiler = do
+  path <- getResourceFilePath
+  iden <- getUnderlying
+  trgt <- getRoute iden
+  unsafeCompiler (callCommand (traceShowId ("exiftool -b -ThumbnailImage "
+                                            ++ path ++ " > "
+                                            ++ fromJust trgt ++ ".jpg")))
+
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "images/*.JPG" $ do
+        route   idRoute
+        compile $ do
+          thumbnailCompiler
+          copyFileCompiler
+
+    match "images/*.jpg" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "images/*.png" $ do
         route   idRoute
         compile copyFileCompiler
 
@@ -30,16 +125,27 @@ main = hakyll $ do
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
-        compile $ pandocCompiler
+        compile $ customPandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
     match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+      route $ setExtension "html"
+      compile $ getResourceBody
+        >>= saveSnapshot "raw_content"
+        >>  customPandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html"    postCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
+
+--     match "posts/*" $ do
+--       route $ gsubRoute "^posts/" (const "gps_tracks/")
+
+    create ["images.js"] $ do
+      route idRoute
+      compile $ do
+        gpsPosCompiler
+
 
     create ["archive.html"] $ do
         route idRoute
@@ -61,20 +167,28 @@ main = hakyll $ do
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
+                    listField "posts" postCtx (return (take 1 posts)) `mappend`
                     constField "title" "Home"                `mappend`
                     defaultContext
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
 
 --------------------------------------------------------------------------------
+
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  dateField "date" "%B %e, %Y" `mappend`
+  bodyField     "body"     `mappend`
+  field "title" (\x -> do
+    let identifier = itemIdentifier x
+    week <- getMetadataField' identifier "week"
+    return (traceShowId ("<span id=\"h_en\">Week " ++ show (round (read week)) ++ " /</span>"
+                         ++ "<span id=\"h_de\"> Woche " ++ show (round (read week)) ++ "</span>"))) `mappend`
+  metadataField            `mappend`
+  urlField      "url"      `mappend`
+  pathField     "path"     `mappend`
+  missingField
